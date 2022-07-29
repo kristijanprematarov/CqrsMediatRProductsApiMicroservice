399683a6d1345154275b937e1a60d3b9ed175cdd
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2022-07-29 19:30:37 +0200

    separate handler

--- CqrsMediatRApi/Commands/AddProductCommand.cs
+++ CqrsMediatRApi/Commands/AddProductCommand.cs
@@ -1,25 +1,6 @@
 ﻿using CqrsMediatRApi.Models;
-using CqrsMediatRApi.Repositories;
 using MediatR;
 
 namespace CqrsMediatRApi.Commands;
 
 public record AddProductCommand(Product Product) : IRequest;
-
-public class AddProductHandler : IRequestHandler<AddProductCommand, Unit>
-{
-    private readonly FakeDataStore _fakeDataStore;
-
-    public AddProductHandler(FakeDataStore fakeDataStore)
-    {
-        _fakeDataStore = fakeDataStore;
-    }
-
-    public async Task<Unit> Handle(AddProductCommand request, CancellationToken cancellationToken)
-    {
-        await _fakeDataStore.AddProduct(request.Product);
-
-        return Unit.Value;
-    }
-}
-
---
+++ CqrsMediatRApi/Handlers/AddProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using CqrsMediatRApi.Commands;
+using CqrsMediatRApi.Repositories;
+using MediatR;
+
+namespace CqrsMediatRApi.Handlers;
+
+public class AddProductHandler : IRequestHandler<AddProductCommand, Unit>
+{
+    private readonly FakeDataStore _fakeDataStore;
+
+    public AddProductHandler(FakeDataStore fakeDataStore)
+    {
+        _fakeDataStore = fakeDataStore;
+    }
+
+    public async Task<Unit> Handle(AddProductCommand request, CancellationToken cancellationToken)
+    {
+        await _fakeDataStore.AddProduct(request.Product);
+
+        return Unit.Value;
+    }
+}
